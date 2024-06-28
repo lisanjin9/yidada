07dbac60624a4a33dbb408d5f32741470f041e77
lisanjin9 <lisanjin9@163.com>
2024-06-28 22:53:19 +0800

    初始化后端模板，使用程序员鱼皮的万用模板

---
+++ yidada-backend/src/main/java/com/sanjin/springbootinit/esdao/PostEsDao.java
@@ -0,0 +1,16 @@
+package com.sanjin.springbootinit.esdao;
+
+import com.sanjin.springbootinit.model.dto.post.PostEsDTO;
+import java.util.List;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 帖子 ES 操作
+ *
+ * 
+ 
+ */
+public interface PostEsDao extends ElasticsearchRepository<PostEsDTO, Long> {
+
+    List<PostEsDTO> findByUserId(Long userId);
+}
---
+++ yidada-backend/src/test/java/com/sanjin/springbootinit/MainApplicationTests.java
@@ -0,0 +1,19 @@
+package com.sanjin.springbootinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 主类测试
+ *
+ * 
+ 
+ */
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
