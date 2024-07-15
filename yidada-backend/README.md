34c79a327a0652cfb8da37c5b536b7f93f555043
lisanjin9 <lisanjin9@163.com>
2024-07-15 21:56:11 +0800

    添加README模板来源

--- yidada-backend/README.md
+++ yidada-backend/README.md
@@ -1,6 +1,5 @@
 # 采用 程序员鱼皮的万用模板(https://www.code-nav.cn/) 
 # SpringBoot 项目初始模板
-22
 
 基于 Java SpringBoot 的项目初始模板，整合了常用框架和主流业务的示例代码。
 
--- yidada-backend/src/main/java/com/sanjin/springbootinit/socring/AiTestScoringStrategy.java
+++ yidada-backend/src/main/java/com/sanjin/springbootinit/socring/AiTestScoringStrategy.java
@@ -101,5 +101,5 @@ public class AiTestScoringStrategy implements ScoringStrategy {
         userMessage.append(JSONUtil.toJsonStr(questionAnswerDTOList));
         return userMessage.toString();
     }
-    
+
 }
