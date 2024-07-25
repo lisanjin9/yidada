245b02ca34c23e631a234d64fa857e06471a2cad
lisanjin9 <lisanjin9@163.com>
2024-07-25 22:42:41 +0800

    部署项目

--- yidada-frontend/src/views/app/AppDetaiPage.vue
+++ yidada-frontend/src/views/app/AppDetaiPage.vue
@@ -29,6 +29,7 @@
             <a-button type="primary" :href="`/answer/do/${id}`"
               >开始答题
             </a-button>
+
             <a-button>分享应用</a-button>
             <a-button v-if="isMy" :href="`/add/question/${id}`"
               >设置题目
